05af91b4fa1e6bc606a0b7f740fa71727ffa2eca
Yanush Dmytro <dmytroyanush@gmail.com>
2025-02-18 10:35:14 +0100

    Added feature() function and myfile.txt to feature branch

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include "Warrior.h"  // comment
 #include <iostream>
 using namespace std;
+
+//added a new comment that exist only in feature branch
+void feature ();
 int main()
 {
     cout << "Hello world!\n";
